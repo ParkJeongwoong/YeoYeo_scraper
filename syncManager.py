95dd742eb51b310f80ad8411982dce9c1457e6d5
dvlprjw <dvlprjw@gmail.com>
2024-08-27 00:33:01 +0900

    fix: timezone

--- syncManager.py
+++ syncManager.py
@@ -120,7 +120,7 @@ def getNaverReservation(monthSize: int)-> tuple:
     bookingList = list({booking['reservationNumber']: booking for booking in bookingList}.values())
     # 체크인 날짜가 오늘 이후인 예약만 남기기
     kst = datetime.timezone(datetime.timedelta(hours=9), 'Asia/Seoul')
-    now = datetime.datetime.now(kst)
+    now = datetime.datetime.now().replace(tzinfo=kst)
     bookingList = list(filter(lambda x: datetime.datetime.strptime(x['startDate'], '%Y%m%d') > now, bookingList))
     print("취소 포함 총 예약 수 : ", len(bookingList))
     print(bookingList)
