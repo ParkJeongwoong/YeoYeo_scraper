bb950cbab2eefa239975a56ed1116e0ccde366d5
dvlprjw <dvlprjw@gmail.com>
2024-12-15 23:23:16 +0900

    feat: 안정성 강화

--- flaskServer.py
+++ flaskServer.py
@@ -1,92 +1,97 @@
 from flask import Flask, jsonify, request
 import syncManager
+import chromeDriver
 
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-activationKey = os.environ.get('ACTIVATION_KEY')
+activationKey = os.environ.get("ACTIVATION_KEY")
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
-def hello_world():
-    return 'Hello, World!'
 
-@app.route('/', methods=['POST'])
+@app.route("/", methods=["GET"])
+def hello_world():
+    return "Hello, World!"
+
+
+@app.route("/", methods=["POST"])
 def hello_world_post():
     req = request.get_json()
-    res = {
-        'message': 'Hello, World!',
-        'data': req
-    }
+    res = {"message": "Hello, World!", "data": req}
     return jsonify(res), 200
 
-@app.route('/sync/in', methods=['POST'])
+
+@app.route("/sync/in", methods=["POST"])
 def sync_naver_reservation():
+    driver = chromeDriver.ChromeDriver()
+    res: dict
+    httpStatus: int
+
     try:
         req = request.get_json()
         # TODO : 마이그레이션 이후 targetDatesStr 삭제 예정
-        targetDatesStr = req['targetDateStr']
+        targetDatesStr = req["targetDateStr"]
         # targetDateStr에서 targetDatesStr로 마이그레이션
-        if req['targetDatesStr'] != None:
-            targetDatesStr = req['targetDatesStr']
-        targetRoom = req['targetRoom']
+        if req["targetDatesStr"] != None:
+            targetDatesStr = req["targetDatesStr"]
+        targetRoom = req["targetRoom"]
         if checkActivationKey(req) == False:
-            res = {
-                'message': 'Invalid Access Key',
-                'data': req
-            }
+            res = {"message": "Invalid Access Key", "data": req}
             return jsonify(res), 401
         print(targetDatesStr, targetRoom)
         syncManager.SyncNaver(targetDatesStr, targetRoom)
-        res = {
-            'message': 'Sync Naver Reservation',
-            'data': req
-        }
-        return jsonify(res), 200
+        res = {"message": "Sync Naver Reservation", "data": req}
+        httpStatus = 200
     except Exception as e:
-        res = {
-            'message': 'Sync Naver Reservation Failed',
-            'error': e
-        }
-        return jsonify(res), 500
+        res = {"message": "Sync Naver Reservation Failed", "error": e}
+        httpStatus = 500
 
-@app.route('/sync/out', methods=['POST'])
+    driver.close()
+    return jsonify(res), httpStatus
+
+
+@app.route("/sync/out", methods=["POST"])
 def get_naver_reservation():
+    driver = chromeDriver.ChromeDriver()
+    res: dict
+    httpStatus: int
+
     try:
         req = request.get_json()
         if checkActivationKey(req) == False:
-            res = {
-                'message': 'Invalid Access Key',
-                'data': {}
-            }
+            res = {"message": "Invalid Access Key", "data": {}}
             return jsonify(res), 401
-        monthSize = req['monthSize']
+        monthSize = req["monthSize"]
         print("monthSize: ", monthSize)
         if monthSize == None:
             monthSize = 1
-        notCanceledBookingList, allBookingList = syncManager.getNaverReservation(monthSize)
+        notCanceledBookingList, allBookingList = syncManager.getNaverReservation(
+            driver, monthSize
+        )
         res = {
-            'message': 'Sync Naver Reservation',
-            'notCanceledBookingList': notCanceledBookingList,
-            'allBookingList': allBookingList
+            "message": "Sync Naver Reservation",
+            "notCanceledBookingList": notCanceledBookingList,
+            "allBookingList": allBookingList,
         }
-        return jsonify(res), 200
+        httpStatus = 200
     except Exception as e:
-        res = {
-            'message': 'Get Naver Reservation Failed',
-            'error': e
-        }
-        return jsonify(res), 500
+        res = {"message": "Get Naver Reservation Failed", "error": e}
+        httpStatus = 500
+
+    driver.close()
+    return jsonify(res), httpStatus
+
 
 def checkActivationKey(req):
-    if 'activationKey' not in req:
+    if "activationKey" not in req:
         return False
-    if activationKey != req['activationKey']:
+    if activationKey != req["activationKey"]:
         return False
     return True
 
-if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", port=5000, debug=True)
--- syncManager.py
+++ syncManager.py
@@ -1,5 +1,4 @@
 import driver
-import chromeDriver
 import simpleManagementController
 import bookingListExtractor
 from random import randint
@@ -10,99 +9,107 @@ import time
 from dotenv import load_dotenv
 import os
 
+
 class RoomType(Enum):
     Yeoyu = 0
     Yeohang = 1
 
+
 # Constant
-naverBizUrl = 'https://nid.naver.com/nidlogin.login?svctype=1&locale=ko_KR&url=https%3A%2F%2Fnew.smartplace.naver.com%2F%3Fnext%3Dbooking-order-management&area=bbt'
-naverLoginUrl = 'https://nid.naver.com/nidlogin.login'
-simpleReservationManagementUrl = 'https://partner.booking.naver.com/bizes/899762/simple-management'
-bookingListUrl = 'https://partner.booking.naver.com/bizes/899762/booking-list-view'
+naverBizUrl = "https://nid.naver.com/nidlogin.login?svctype=1&locale=ko_KR&url=https%3A%2F%2Fnew.smartplace.naver.com%2F%3Fnext%3Dbooking-order-management&area=bbt"
+naverLoginUrl = "https://nid.naver.com/nidlogin.login"
+simpleReservationManagementUrl = (
+    "https://partner.booking.naver.com/bizes/899762/simple-management"
+)
+bookingListUrl = "https://partner.booking.naver.com/bizes/899762/booking-list-view"
 
 load_dotenv()
 
-id = os.environ.get('ID')
-pw = os.environ.get('PASSWORD')
+id = os.environ.get("ID")
+pw = os.environ.get("PASSWORD")
 
 # Temporary Variable
 # targetDatesStr = '2024-09-02,2024-09-03'
 # targetRoom = RoomType.Yeohang
 
+
 def randomSleep(dirver: driver.Driver):
-    sleepTime = randint(15, 30)/10
-    print(f'Random Sleep: {sleepTime}')
+    sleepTime = randint(15, 30) / 10
+    print(f"Random Sleep: {sleepTime}")
     dirver.wait(sleepTime)
 
+
 def randomRealSleep():
-    sleepTime = randint(15, 30)/5
-    print(f'Long Sleep: {sleepTime}')
+    sleepTime = randint(15, 30) / 5
+    print(f"Long Sleep: {sleepTime}")
     time.sleep(sleepTime)
 
-def makeTargetDateList(dateListStr: str)->list:
-    dateList = dateListStr.split(',')
+
+def makeTargetDateList(dateListStr: str) -> list:
+    dateList = dateListStr.split(",")
     dateList.sort()
     targetDateList = list(map(lambda x: makeTargetDate(x), dateList))
     return targetDateList
-    
-def makeTargetDate(dateStr: str)->datetime.date:
-    dateList = dateStr.split('-')
+
+
+def makeTargetDate(dateStr: str) -> datetime.date:
+    dateList = dateStr.split("-")
     return datetime.date(int(dateList[0]), int(dateList[1]), int(dateList[2]))
 
-def SyncNaver(targetDateStr: str, targetRoom: str):
+
+def SyncNaver(driver: driver.Driver, targetDateStr: str, targetRoom: str):
     targetRoom = RoomType[targetRoom]
 
     reservationManager = simpleManagementController.SimpleManagementController()
-    driver = chromeDriver.ChromeDriver()
     driver.goTo(naverLoginUrl)
-    print('네이버 로그인 페이지 이동')
+    print("네이버 로그인 페이지 이동")
 
     # 로그인
     driver.login(id, pw)
 
-    driver.findBySelector('#log\.login').click()
-    print('로그인 성공')
+    driver.findBySelector("#log\.login").click()
+    print("로그인 성공")
     randomSleep(driver)
     randomRealSleep()
 
     driver.goTo(simpleReservationManagementUrl)
-    print('간단예약관리 페이지 이동')
+    print("간단예약관리 페이지 이동")
     randomSleep(driver)
     randomRealSleep()
 
     # 날짜 변경
     targetDateList = makeTargetDateList(targetDateStr)
     for targetDate in targetDateList:
-        print(targetDate, '예약 상태 변경 시작')
+        print(targetDate, "예약 상태 변경 시작")
         idxOfDate = reservationManager.findTargetPage(driver, targetDate)
         if idxOfDate == -1:
-            print('해당 날짜가 존재하지 않습니다.')
-            print(targetDate, '예약 변경 실패')
+            print("해당 날짜가 존재하지 않습니다.")
+            print(targetDate, "예약 변경 실패")
             driver.close()
             return
-        
+
         # 예약 상태 변경
-        print(targetDate, '예약 상태 변경 중')
+        print(targetDate, "예약 상태 변경 중")
         reservationManager.findTargetBtn(driver, idxOfDate, targetRoom.value).click()
 
         randomSleep(driver)
-        print(targetDate, targetRoom.name, '예약 변경 완료')
+        print(targetDate, targetRoom.name, "예약 변경 완료")
     driver.close()
-    
-def getNaverReservation(monthSize: int)-> tuple:
-    driver = chromeDriver.ChromeDriver()
+
+
+def getNaverReservation(driver: driver.Driver, monthSize: int) -> tuple:
     driver.goTo(naverLoginUrl)
     # 로그인
 
     driver.login(id, pw)
 
-    driver.findBySelector('#log\.login').click()
-    print('로그인 성공')
+    driver.findBySelector("#log\.login").click()
+    print("로그인 성공")
     randomSleep(driver)
     randomRealSleep()
 
     driver.goTo(bookingListUrl)
-    print('예약자관리 페이지 이동')
+    print("예약자관리 페이지 이동")
     randomSleep(driver)
     randomSleep(driver)
     randomRealSleep()
@@ -110,27 +117,43 @@ def getNaverReservation(monthSize: int)-> tuple:
     # 예약자 정보 가져오기
     bookingList = []
     for i in range(monthSize):
-        print(f'{i+1}번째 월 예약자 정보 가져오기 시작')
+        print(f"{i+1}번째 월 예약자 정보 가져오기 시작")
         randomSleep(driver)
-        monthBookingList = bookingListExtractor.extractBookingList(driver.getPageSource())
+        monthBookingList = bookingListExtractor.extractBookingList(
+            driver.getPageSource()
+        )
         print("length: ", len(monthBookingList))
         print(monthBookingList)
         bookingList.extend(monthBookingList)
-        driver.findByXpath('//button[contains(@class, "DatePeriodCalendar__next")]').click()
+        driver.findByXpath(
+            '//button[contains(@class, "DatePeriodCalendar__next")]'
+        ).click()
         randomRealSleep()
     # bookingList에서 중복 제거
-    bookingList = list({booking['reservationNumber']: booking for booking in bookingList}.values())
+    bookingList = list(
+        {booking["reservationNumber"]: booking for booking in bookingList}.values()
+    )
     # 체크인 날짜가 오늘 이후인 예약만 남기기
-    kst = datetime.timezone(datetime.timedelta(hours=9), 'Asia/Seoul')
+    kst = datetime.timezone(datetime.timedelta(hours=9), "Asia/Seoul")
     now = datetime.datetime.now(datetime.timezone.utc).astimezone(kst)
     for booking in bookingList:
-        start = datetime.datetime.strptime(booking['startDate'], '%Y%m%d').replace(tzinfo=kst)
-        print(f'{start} {now} {start > now}')
-    bookingList = list(filter(lambda x: datetime.datetime.strptime(x['startDate'], '%Y%m%d').replace(tzinfo=kst) > now, bookingList))
+        start = datetime.datetime.strptime(booking["startDate"], "%Y%m%d").replace(
+            tzinfo=kst
+        )
+        print(f"{start} {now} {start > now}")
+    bookingList = list(
+        filter(
+            lambda x: datetime.datetime.strptime(x["startDate"], "%Y%m%d").replace(
+                tzinfo=kst
+            )
+            > now,
+            bookingList,
+        )
+    )
     print("취소 포함 총 예약 수 : ", len(bookingList))
     print(bookingList)
     driver.close()
-    notCanceledBookingList = list(filter(lambda x: x['status'] != '취소', bookingList))
+    notCanceledBookingList = list(filter(lambda x: x["status"] != "취소", bookingList))
     print("취소 미포함 확정 예약 수 : ", len(notCanceledBookingList))
     print(notCanceledBookingList)
     return notCanceledBookingList, bookingList
